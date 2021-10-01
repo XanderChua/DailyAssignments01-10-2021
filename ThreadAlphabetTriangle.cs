dcb4fed113273aad048a9f07bb935d93408a5c86
XanderChua <72645612+XanderChua@users.noreply.github.com>
2021-10-01 15:31:25 +0800

    Add files via upload

---
+++ ThreadAlphabetTriangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadAlphabetTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadClass t = new ThreadClass();
+            t.Callthread();
+        }
+        class ThreadClass
+        {
+            public void Callthread()
+            {
+                Thread t = new Thread(Display);
+                t.Start();
+            }
+            public void Display()
+            {
+                Console.WriteLine("Enter number from 1 to 26: ");
+                int input = Int32.Parse(Console.ReadLine());
+                for(int i = 1; i < input+1; i++)
+                {
+                    for(int j = 1; j < input+1 - i; j++)//every loop space decrease 1
+                    {
+                        Console.Write(" ");
+                    }
+                    char incline = 'A';
+                    Thread.Sleep(1000);
+                    for (int k = 1; k <= i; k++)
+                    {
+                        Console.Write(incline++);
+                    }
+                    char decline = (char)(incline - 2);
+                    for (int l = 1; l < i; l++)
+                    {
+                        Console.Write(decline--);
+                    }
+                    Console.Write("\n");
+                }              
+            }           
+        }
+    }
+}
---
+++ ThreadTriangleSum.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadTriangleSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadClass t = new ThreadClass();
+            t.Callthread();
+
+        }
+        class ThreadClass
+        {
+            public void Callthread()
+            {
+                Thread t = new Thread(Display);
+                t.Start();
+            }
+            public void Display()
+            {               
+                Console.WriteLine("Enter the limit: ");
+                int input = Int32.Parse(Console.ReadLine());
+                int[] numArray = new int[input+1];
+                for (int i = 1; i < numArray.Length; i++)
+                {
+                    for(int j = 1; j <= i; j++)
+                    {
+                        Thread.Sleep(300);
+                        Console.Write(IdkWhatMethodIsThiszxc(j) + " ");
+                    }
+                    Console.Write("\n");
+                    Thread.Sleep(1000);
+                }
+            }
+            static int IdkWhatMethodIsThiszxc(int number)
+            {
+                int first = 0;
+                int second = 1;
+                int result = 0;
+                if (number == 0)
+                {
+                    return number;
+                }
+                if (number == 1)
+                {
+                    return number;
+                }
+                for (int i = 2; i <= number; i++)  
+                {
+                    result = first + second;
+                    first = second;
+                    second = result;
+                }
+                return result;
+            }
+        } 
+    }
+}
